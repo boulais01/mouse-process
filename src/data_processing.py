fdfaca0e7151fb2336c287b1faba0de5b8cac4f6
boulais01 <bookdragon1202@gmail.com>
2025-03-14 14:14:30 -0400

    feat: finish info process

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,40 +2,59 @@
 
 import json
 import csv
+import pandas as pd
 import os
 from pathlib import Path
 
 path = Path("research-data/data")
 
+
 # list of lists; each list a line, do each line data from list in notebook
 # passage, time between mouse moves, distance between mouse moves, quadrant(s)
 #               ^out of total passage time?
 # TODO: get all of my desired values, then determine how to csv it.
-passage_sorting = {}
+#passage_sorting = {}
 for folder in path.iterdir():
-    cvs_file = os.getcwd() + "\\processed\\" + str(folder) + ".csv"
+    cvs_file = os.getcwd() + "\\processed\\" + str(folder)
     for file in folder.iterdir():
         if file.match("info*"):
-            with open(file, "r") as i_file:
-                data = json.load(i_file)
-                for i in data:
-                    # TODO: sort passages into order
-                    print(i)
-                    if i["passage"] not in passage_sorting.keys():
-                        passage_sorting[i["passage"]] = [i]
-                    else:
-                        passage_sorting[i["passage"]].append(i)
-                        print(passage_sorting[i["passage"]])
-            #with open(cvs_file, "w") as writeto:
-             #   writer = csv.writer(writeto)
-              #  for j in passage_sorting:
-                    #print(j)
-               #     writer.writerow(j)
-            # write to one csv
+            with file.open("r", encoding="utf-8") as i_file:
+                # read json
+                data = json.loads(i_file.read())
+
+                # create dataframe
+                df = pd.json_normalize(data)
+                
+                # save to csv
+                df.to_csv(cvs_file + "_moves.csv", index=False, encoding="utf-8")
         elif file.match("states*"):
-            with open(file, "r") as s_file:
-                data = json.load(s_file)
-            # write to another csv
+            states_sorting = {}
+            with file.open("r", encoding="utf-8") as s_file:
+                # read json
+                data = json.loads(s_file.read())
+
+                for i in range(len(data) - 1):
+                    if isinstance(data[i], list) or isinstance(data[i], dict):
+                        if len(data[i]) < 30:
+                            states_sorting[i] = data[i]
+                    else:
+                        states_sorting[i] = data[i]
+                # TODO: 4 and 5 are just copies of width and height, try/except for values > 5 and values < len(data)
+                # that puts all the unlabeled vars in a list, compare to relevant values in listed passages to determine identity
+
+                # TODO: also time math
+
+                # create dataframe
+                #df = pd.json_normalize(data)
+                
+                # save to csv
+                #df.to_csv(cvs_file + "_all.csv", index=False, encoding="utf-8")
+                print(states_sorting)
+                #with open(cvs_file + "_states.csv", "w") as writeto:
+                   #writer = csv.writer(writeto)
+                   #for j in states_sorting:
+                        #print(states_sorting[j])
+                        #writer.writerow(j)
             break
 
         
