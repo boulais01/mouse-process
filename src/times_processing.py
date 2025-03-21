97149eeb38581a20ffb9061cfb09bf8fd48c864e
boulais01 <bookdragon1202@gmail.com>
2025-03-20 23:07:06 -0400

    feat: add collector, comments

--- src/times_processing.py
+++ src/times_processing.py
@@ -4,18 +4,26 @@ import csv
 from pathlib import Path
 
 path = Path("processed/research-data/data")
-
+all_passages = []
 
 for file in path.iterdir():
+    # locate the files with the mouse movements
     if file.match("*_moves.csv"):
         passage_sort = {}
         reader = csv.DictReader(open(file))
+        # iterate through each instance of movement
         for row in reader:
+            # sort the movement into a list for each passage
+            if row["passage"] not in all_passages:
+                all_passages.append(row["passage"])
             try:
                 passage_sort[row["passage"]].append({"time": int(row["time"]), "mouseX": int(row["mouseX"]), "mouseY": int(row["mouseY"])})
             except KeyError:
                 passage_sort[row["passage"]] = [{"time": int(row["time"]), "mouseX": int(row["mouseX"]), "mouseY": int(row["mouseY"])}]
         sorted_passages = {}
+        # order the list
         for passage in passage_sort.keys():
             sorted_passages[passage] = sorted(passage_sort[passage], key=lambda x: x["time"])
+        
+print(all_passages)
         
